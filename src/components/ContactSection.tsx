32cddf1d5dbb077e0d863b54b21d50874f361c49
vinicius <vinicius.lldavid@gmail.com>
2025-04-29 17:20:50 -0300

    Add conexao com backend

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -23,7 +23,7 @@ const ContactSection: React.FC = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/send', {
+      const response = await fetch('https://atoeenegiabackend.onrender.com/send', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
